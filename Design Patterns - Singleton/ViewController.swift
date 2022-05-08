34e8078cd5755461b87c45b5d702a83a9f6a64f6
Frantisek Varadzin <varadzin@gmail.com>
2022-05-08 08:37:50 +0200

    Initial Commit

---
+++ Design Patterns - Singleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design Patterns - Singleton
+//
+//  Created by Frantisek Varadzin on 08.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
